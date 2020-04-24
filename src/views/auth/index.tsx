a1f1c6a0e06adfecbb54cb8609d605f9ee1fc63c
lukigarazus <ywnwa96@gmail.com>
2020-04-24 22:43:30 +0200

    Auth styling

--- src/views/auth/index.tsx
+++ src/views/auth/index.tsx
@@ -1,23 +1,32 @@
 import React, { useContext, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import { FirebaseContext } from "../../firebase";
 
+const useStyles = makeStyles((theme) => ({
+  authCont: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+}));
+
 export default () => {
+  const classes = useStyles();
   const [state, setState] = useState<{ error?: string }>({ error: undefined });
   const firebaseContext = useContext(FirebaseContext);
 
   async function googleSignIn() {
     try {
       // @ts-ignore
-      console.log(await firebaseContext.signinWithGoogle());
+      await firebaseContext.signinWithGoogle();
     } catch (error) {
       setState({ error: error.message });
     }
   }
 
   return (
-    <div>
-      Auth
+    <div className={classes.authCont}>
       <div>
         <Button onClick={googleSignIn}>Sign in with Google</Button>
         <div>{state.error}</div>
